e2327c30f8c62cc36920a47d7bda980aff6e82b2
franklin-sula <franklin.sula09@gmail.com>
2024-10-21 20:29:11 +0800

    Update AdminSidebar.jsx

--- src/components/admin/AdminSidebar.jsx
+++ src/components/admin/AdminSidebar.jsx
@@ -17,7 +17,7 @@ const adminLinks = [
   { link: "/admin-dashboard", label: "Dashboard", icon: DashboardIcon },
   { link: "/attendance", label: "Attendance", icon: CheckListIcon },
   { link: "/groups", label: "Groups", icon: PersonIcon },
-  { link: "/schedule", label: "Schedule", icon: CalendarIcon },
+  { link: "/event", label: "Schedule", icon: CalendarIcon },
   { link: "/admin-calendar", label: "Calendar", icon: CalendarIcon },
   { link: "/volunteers", label: "Request(s)", icon: RequestIcon },
 ];
